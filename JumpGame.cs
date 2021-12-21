7885a5a1e651b4e8a017b7d589e3d84f4a75b08a
vadzimshypko <vadimshipko@gmail.com>
2021-12-21 12:26:59 +0100

    Jump-game

---
+++ JumpGame.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode
+{
+    /**
+     * https://leetcode.com/problems/jump-game/
+     */
+    internal class JumpGame : Solution
+    {
+        public void Run()
+        {
+            Console.WriteLine("nums = ?,?,?");
+            int[] nums = Console.ReadLine().Split(",").Select(int.Parse).ToArray();
+            Console.WriteLine(Solve(nums));
+        }
+
+        public void RunWithDefaultArguments()
+        {
+            Console.WriteLine(Solve(new[] { 3, 2, 1, 0, 4 }));
+        }
+
+        private bool Solve(int[] nums)
+        {
+            int largestAvailableIndex = 0;
+            int lastIndex = nums.Length - 1;
+            for (int i = 0; i <= largestAvailableIndex && largestAvailableIndex < lastIndex; i++)
+            {
+                largestAvailableIndex = Math.Max(largestAvailableIndex, i + nums[i]);
+            }
+            return nums.Length - 1 <= largestAvailableIndex;
+        }
+    }
+}
